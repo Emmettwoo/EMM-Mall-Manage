91c3b833d679e89f280efedf2286e3eb3bad9e5d
Emmett Woo <Emmettwoo@Outlook.com>
2020-04-18 20:03:20 +0800

    init
    脚手架

---
+++ README.md
@@ -0,0 +1,25 @@
+# EMM-Mall: 从零开始的个人电商网站开发旅程
+
+
+## Intro.简介
+
+![Features](/wiki/Features.png)
+
+---
+
+## Version.版本
+
+- [x] V0.0.0 Project Start 脚手架的搭建
+- [ ] V0.1.0 [Common Function](#) 通用功能
+- [ ] V0.2.0 [User Module](#) 基础模块
+- [ ] V0.3.0 [Product Module](#) 商品模块
+- [ ] V0.4.0 [Cart Module](#) 品类模块
+- [ ] V0.5.0 [Shipping Module](#) 订单模块
+- [ ] V0.6.0 Architecture Adjustment 架构调整
+- [ ] V0.7.0 Fixme Tasks Fix 细节修复
+
+---
+
+## Thanks.鸣谢
+
+- [慕课网](https://coding.imooc.com/class/179.html)
---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: "./src/demo.jsx",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "/dist/",
+        filename: "js/demo.js"
+    },
+    module: {
+        rules: [
+            // jsx脚本加载
+            {
+                test: /\.jsx$/,
+                exclude: /(nide_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["env", "react"]
+                    }
+                }
+            },
+            // css样式加载
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            },
+            // scss样式加载
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ["css-loader", "sass-loader"]
+                })
+            },
+            // 图片文件加载
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [{
+                    loader: "url-loader",
+                    options: {
+                        name: "resource/image/[name].[ext]",
+                        limit: 8192
+                    }
+                }]
+            },
+            // 字体图标加载
+            {
+                test: /\.(eot|svg|woff|woff2|ttf|otf)$/,
+                use: [{
+                    loader: "url-loader",
+                    options: {
+                        name: "resource/font/[name].[ext]",
+                        limit: 8192
+                    }
+                }]
+            }
+        ]
+    },
+    plugins: [
+        // Html处理插件
+        new HtmlWebpackPlugin({
+            template: "./src/demo.html",
+            filename: "view/index.html"
+        }),
+        // CSS处理插件
+        new ExtractTextPlugin("css/[name].css"),
+        // 代码分离插件
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "common",
+            filename: "js/base.js",
+        })
+    ],
+    devServer: {
+        port: 8088
+    }
+};
